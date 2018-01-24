7b1f84e5b572b8040757885e2634ed83613f2c63
Urlandi <letitbee@elsv-v.ru>
2018-01-24 14:31:34 +0300

    Script for crontab

---
+++ bot_cronjob.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o nounset
+
+declare -r BASE_DIR='/opt/BGP-Table'
+declare -r BOT_FILE='bgptable_tw2tg.py'
+declare -r NOT_FOUND=1
+
+ps ax | grep $BOT_FILE$
+
+process_found=$?
+
+if [ $process_found -eq $NOT_FOUND ]; then
+    python3 $BASE_DIR/$BOT_FILE&
+fi

1e6d0b930e9f72505a811af4cd64fe088af349f5
Urlandi <letitbee@elsv-v.ru>
2018-01-25 11:12:57 +0300

    Strongest variable usage

--- bot_cronjob.sh
+++ bot_cronjob.sh
@@ -6,10 +6,10 @@ declare -r BASE_DIR='/opt/BGP-Table'
 declare -r BOT_FILE='bgptable_tw2tg.py'
 declare -r NOT_FOUND=1
 
-ps ax | grep -q $BOT_FILE$
+ps ax | grep -q ${BOT_FILE}$
 
 process_found=$?
 
-if [ $process_found -eq $NOT_FOUND ]; then
-    python3 $BASE_DIR/$BOT_FILE&
+if [ ${process_found} -eq ${NOT_FOUND} ]; then
+    python3 ${BASE_DIR}/${BOT_FILE}&
 fi
